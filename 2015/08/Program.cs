7da66b5a8dbf05ac9b036ea4bd2d3f710b74028b
AU0007KT <martin.rustler@sivantos.com>
2020-12-08 18:11:36 +0100

    Day 8 2015

---
+++ 2015/08/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input");
+
+            var sumOfOriginalLineLengths = input.Sum(line => line.Length);
+            var sumOfRealLineLengths = input.Sum(line => Regex.Unescape(line.Substring(1, line.Length - 2)).Length);
+            var sumOfEscapedLineLengths = input.Sum(line => $"\"{Escape(line)}\"".Length);
+
+            var solutionPart1 = sumOfOriginalLineLengths - sumOfRealLineLengths;
+            var solutionPart2 = sumOfEscapedLineLengths - sumOfOriginalLineLengths;
+
+            Console.WriteLine($"Solution Part 1: {solutionPart1}");
+            Console.WriteLine($"Solution Part 2: {solutionPart2}");
+
+            string Escape(string input)
+            {
+                return input.Replace("\\", "\\\\").Replace("\"", "\\\"");
+            }
+        }
+    }
+}
